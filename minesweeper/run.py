22ec9f31aaf0c0a47ec9cb0edef3e016a514bf26
Niel Conradie <nielconradie93@gmail.com>
2022-06-03 15:15:59 +0200

    Create run.py
    Added run.py file to run game.

---
+++ minesweeper/run.py
@@ -0,0 +1,13 @@
+from minesweeper import Minesweeper
+
+
+def run():
+    """ Minesweeper. """
+    run = Minesweeper()
+
+    # Starting the game.
+    run.start_game()
+
+
+if __name__ == '__main__':
+    run()
