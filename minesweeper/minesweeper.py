d07cf9692de924f143c855201e16b365c70bd66e
Niel Conradie <nielconradie93@gmail.com>
2022-06-03 15:14:01 +0200

    Update minesweeper.py
    Object-Oriented Programming Implementation.

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -1,44 +1,65 @@
 from board import Board
 
 
-def play(dimension_size=10, number_bombs=10):
-    """ Minesweeper. """
-    # Creating the board.
-    board = Board(dimension_size, number_bombs)
-    safe = True
+class Minesweeper:
+    """ A class to represent a minesweeper game. """
 
-    while len(board.dug) < board.dimension_size ** 2 - number_bombs:
-        print()
-        print(board)
+    @staticmethod
+    def user_input_row():
+        """ Requesting user input and validating number. """
+        while True:
+            try:
+                user_input = int(input("\nSelect row: "))
+                return user_input
+            except ValueError:
+                print(f"\nThat is not a number.")
+                continue
 
-        # Requesting user input and validating input.
-        user_input = input(
-            "\nWhere would you like to dig? Input as row, column: ")
-        user_input = user_input.split(' ')
-        row, column = int(user_input[0]), int(user_input[-1])
-        if row < 0 or row >= board.dimension_size or column < 0 or column >= dimension_size:
-            print("\nInvalid location. Please try again.")
-            continue
+    @staticmethod
+    def user_input_column():
+        """ Requesting user input and validating number. """
+        while True:
+            try:
+                user_input = int(input("\nSelect column: "))
+                return user_input
+            except ValueError:
+                print(f"\nThat is not a number.")
+                continue
 
-        # Verifying a valid dig.
-        safe = board.dig(row, column)
-        if not safe:
-            break
+    def start_game(self, dimension_size=10, number_bombs=10):
+        """ Start Minesweeper game. """
+        # Creating the board.
+        board = Board(dimension_size, number_bombs)
+        safe = True
 
-    # Game Won/Lost conditions.
-    if safe:
-        print()
-        board.dug = [(r, c) for r in range(board.dimension_size)
-                     for c in range(board.dimension_size)]
-        print(board)
-        print("\nWINNER!")
-    else:
-        print()
-        board.dug = [(r, c) for r in range(board.dimension_size)
-                     for c in range(board.dimension_size)]
-        print(board)
-        print("\nGAME OVER!")
+        while len(board.dug) < board.dimension_size ** 2 - number_bombs:
+            print()
+            print(board)
 
+            print("\nWhere would you like to dig?")
 
-if __name__ == '__main__':
-    play()
+            # Requesting user input and validating number.
+            row = self.user_input_row()
+            column = self.user_input_column()
+            if row < 0 or row >= board.dimension_size or column < 0 or column >= dimension_size:
+                print("\nInvalid location. Please try again.")
+                continue
+
+            # Verifying a valid dig.
+            safe = board.dig(row, column)
+            if not safe:
+                break
+
+        # Win conditions.
+        if safe:
+            print()
+            board.dug = [(r, c) for r in range(board.dimension_size)
+                         for c in range(board.dimension_size)]
+            print(board)
+            print("\nWINNER!")
+        else:
+            print()
+            board.dug = [(r, c) for r in range(board.dimension_size)
+                         for c in range(board.dimension_size)]
+            print(board)
+            print("\nGAME OVER!")
