e11dc045d2c8beed62577fe93eb39ac3b19bf492
Niel Conradie <nielconradie93@gmail.com>
2022-06-16 10:42:12 +0200

    Update minesweeper.py
    Added restart method to class.

--- minesweeper/minesweeper.py
+++ minesweeper/minesweeper.py
@@ -63,3 +63,20 @@ class Minesweeper:
                          for c in range(board.dimension_size)]
             print(board)
             print("\nGAME OVER!")
+
+    @staticmethod
+    def restart():
+        """ Requesting user input and validating choice. """
+        while True:
+            user_input = input("\nRestart? Yes/No: ").lower()
+            choices = ['yes', 'no']
+            if user_input not in choices:
+                print("\nPlease type 'yes' or 'no'")
+                continue
+
+            # User input conditions.
+            if user_input == 'yes':
+                return
+            if user_input == 'no':
+                print("\nThank you for playing!")
+                quit()
