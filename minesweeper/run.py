e7ee86f99fd396c1c7d5b694502f50a19dc05ebc
Niel Conradie <nielconradie93@gmail.com>
2022-07-01 11:04:46 +0200

    Update run.py
    Added new methods to run.py file.

--- minesweeper/run.py
+++ minesweeper/run.py
@@ -1,4 +1,3 @@
-from board import Board
 from minesweeper import Minesweeper
 
 
@@ -6,11 +5,18 @@ def run():
     """Minesweeper."""
     run = Minesweeper()
 
+    # Display difficulty options.
+    run.display_difficulty()
+    # Requesting user input.
+    user_input = run.user_input()
+    # Assign player to appropriate difficulty.
+    size, bombs = run.user_input_allocation(user_input)
+
     while True:
         # Display scoreboard.
         run.display_scoreboard()
         # Starting game.
-        run.start_game()
+        run.start_game(size, bombs)
         # Add point to round.
         run.add_round()
         # Requesting user input.
