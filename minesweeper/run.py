1b3897e473f639bfdbb1fd80f011e3e98359427d
Niel Conradie <nielconradie93@gmail.com>
2022-07-28 14:31:59 +0200

    Update run.py
    Removed old unused code and added start_game() method.

--- minesweeper/run.py
+++ minesweeper/run.py
@@ -5,24 +5,8 @@ def run():
     """Minesweeper."""
     run = Minesweeper()
 
-    # Display difficulty options.
-    run.display_difficulty()
-    # Requesting user input.
-    user_input = run.user_input()
-    # Assign player to appropriate difficulty.
-    size, bombs = run.user_input_allocation(user_input)
-
-    while True:
-        # Display scoreboard.
-        run.display_scoreboard()
-        # Starting game.
-        run.start_game(size, bombs)
-        # Add point to round.
-        run.add_round()
-        # Requesting user input.
-        run.restart()
-
-        continue
+    # Starting the game.
+    run.start_game()
 
 
 if __name__ == "__main__":
